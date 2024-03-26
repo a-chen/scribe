4f2263f9130eaf31f7f1d676ed5b6ff8c177bda9
Andrew Chen <achen.this@gmail.com>
2024-03-26 17:48:09 -0400

    Initial working version

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,15 @@ Copyright © 2024 Andrew Chen <achen.this@gmail.com>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -16,16 +23,104 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "scribe",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "CLI app that takes in audio/video files, YouTube links, and produces transcribed text",
+	Long: `CLI app that takes in audio/video files, YouTube links, and produces transcribed text
+Depending on your ASR source, transcription will take a couple of minutes
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+USAGE
+scribe someYouTubeLink | output.txt 
+scribe someAudioFile.mp3
+scribe someVideoFile.mp4`,
+	Args: cobra.ExactArgs(1),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		var filePath = args[0]
+		var audioFilePath = "tmp.mp3"
+		// cleanup tmp file
+		os.Remove(audioFilePath)
+
+		//log.Printf("Processing %s\n", filePath)
+
+		if strings.Contains(filePath, "youtube.com") {
+			// extract audio file
+			//log.Println("Extracting audio file...")
+			cmd := exec.Command("yt-dlp", "--extract-audio", "--audio-format", "mp3", "--audio-quality", "10", "-o", audioFilePath, filePath)
+			err := cmd.Run()
+			if err != nil {
+				log.Fatalf("Command execution failed with error: %v", err)
+			}
+		} else if strings.HasSuffix(filePath, ".mp3") {
+			// feed audio file in
+			audioFilePath = filePath
+		} else {
+			//log.Println("Extracting audio file...")
+			cmd := exec.Command("ffmpeg", "-i", filePath, "-vn", "-acodec", "libmp3lame", audioFilePath)
+
+			err := cmd.Run()
+			if err != nil {
+				log.Fatalf("Command execution failed with error: %v", err)
+			}
+		}
+
+		// call WhisperASR
+		// Prepare a form that you will submit via that POST.
+		buf := new(bytes.Buffer)
+		writer := multipart.NewWriter(buf)
+		formFile, err := writer.CreateFormFile("audio_file", audioFilePath)
+		if err != nil {
+			log.Fatalf("Create form file failed: %s\n", err)
+		}
+
+		// Open the file
+		fh, err := os.Open(audioFilePath)
+		if err != nil {
+			log.Fatalf("Error opening file: %v", err)
+		}
+		defer fh.Close()
+
+		_, err = io.Copy(formFile, fh)
+		if err != nil {
+			log.Fatalf("Error copying data to form file: %v", err)
+		}
+
+		// Close multipart writer.
+		writer.Close()
+
+		// Create a client
+		client := &http.Client{}
+
+		var asrServerUrl = "http://0.0.0.0:9000"
+		// Create a New Request
+		// see https://ahmetoner.com/whisper-asr-webservice/endpoints/#automatic-speech-recognition-service-asr
+		req, err := http.NewRequest(
+			"POST",
+			asrServerUrl+"/asr",
+			buf,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+
+		// Do the request
+		//log.Println("Transcribing...")
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		//log.Println("Transcription complete")
+		bodyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		fmt.Print(bodyString)
+		// cleanup tmp file
+		os.Remove(audioFilePath)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version",
+	Long:  `Prints the version`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v1.0.0")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
